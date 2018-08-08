3f57bbdc35c5919844bff427d20e6563a7749e86
Joshua M Smith <josh7777smith@gmail.com>
2018-08-08 18:32:43 -0400

    add venv to gitignore

---
+++ services/users/project/__init__.py
@@ -0,0 +1,13 @@
+# services/users/project/__init__.py
+
+from flask import Flask, jsonify
+
+# instantiate the app
+app = Flask(__name__)
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
