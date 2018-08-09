222ddc9bc42dddf3d27d716ee74be11f4e508ad4
Joshua M Smith <josh7777smith@gmail.com>
2018-08-08 20:28:59 -0400

    update python for db

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,7 +3,7 @@
 import os
 # import sys
 from flask import Flask, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate the app
 app = Flask(__name__)
@@ -13,7 +13,24 @@ app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 # print(app.config, file=sys.stderr)
 
+# Instantiate the database
+db = SQLAlchemy(app)
 
+
+# Model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+# Routes
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
